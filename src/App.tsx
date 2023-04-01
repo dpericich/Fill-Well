a8801fed293b9eb357530e851d353f6e74a4a546
Daniel Pericich <danielpericich@Daniels-MacBook-Pro.local>
2023-03-31 23:28:16 -0500

    updated App component with react router logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+import ProductionSchedule from './pages/ProductionSchedule';
+import LPPackout from './pages/LPPackout';
+import SPPackout from './pages/SPPackout';
+import Hoses from './pages/Hoses';
 
 function App() {
+  const BackgroundContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    background-color: black;
+  `
+
+  const AppContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    text-align: center;
+    background-color: white;
+    max-width: 850px;
+    width: 100%;
+    padding: 1rem 0;
+    height: 100vh;
+  `
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BackgroundContainer>
+      <AppContainer>
+        <Header />
+        <Router>
+          <Routes>
+            <Route element={<ProductionSchedule />} path="production-schedule"/>
+            <Route element={<LPPackout />} path="/lp-packout"/>
+            <Route element={<SPPackout />} path="/sp-packout"/>
+            <Route element={<Hoses />} path="/hoses"/>
+            <Route element={<Home />} path="/"/>
+          </Routes>
+        </Router>
+        <Footer />
+      </AppContainer>
+    </BackgroundContainer>
   );
 }
 
