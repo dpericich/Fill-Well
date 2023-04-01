c61ce7e66624f195a831bfa2344c6fe737043067
Daniel Pericich <danielpericich@Daniels-MacBook-Pro.local>
2023-03-31 23:20:42 -0500

    stubbed out loading spinner

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,9 @@
+import React, { FC, ReactElement } from 'react';
+
+const LoadingSpinner: FC = (): ReactElement => {
+    return(
+        <div>I am a Spinner</div>
+    )
+};
+
+export default LoadingSpinner;
