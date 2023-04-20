4560670bf08bac3ceb196145444a4631ae87a470
Daniel Pericich <dpericich@gmail.com>
2023-04-20 00:16:49 -0500

    scaffoled API calls for LP and SP Packout list screens

--- src/pages/LPPackout.tsx
+++ src/pages/LPPackout.tsx
@@ -1,8 +1,14 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useEffect, useState } from 'react';
 
 const LPPackout: FC = (): ReactElement => {
-    // have to have a useEffect to load initial list of items from the back end
+    const [packouts, setPackouts] = useState([])
+    
+    useEffect(() => {
+        // have to have a useEffect to load initial list of items from the back end
+    }, [])
+
     return(
+        // Will want to make this a CSS Grid to make it 2D responsive
         <div>Hello</div>
     )
 };
--- src/pages/SPPackout.tsx
+++ src/pages/SPPackout.tsx
@@ -1,7 +1,14 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useState, useEffect } from 'react';
 
 const SPPackout: FC = (): ReactElement => {
+    const [packouts, setPackouts] = useState([])
+    
+    useEffect(() => {
+        // have to have a useEffect to load initial list of items from the back end
+    }, [])
+
     return(
+        // Will want to make this a CSS Grid to make it 2D responsive
         <div>Hello</div>
     )
 };
