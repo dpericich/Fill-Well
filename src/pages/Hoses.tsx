777548cb2a5e204461d242fcafe688db10c0fa87
Daniel Pericich <dpericich@gmail.com>
2023-04-22 12:20:50 -0500

    added grid and data logic to hoses component

--- src/pages/Hoses.tsx
+++ src/pages/Hoses.tsx
@@ -1,8 +1,35 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useState, useEffect } from 'react';
+import styled from 'styled-components';
+import Button from '../components/Button';
+import { options } from '../data/hoses_data';
 
 const Hoses: FC = (): ReactElement => {
+    const [packouts, setPackouts] = useState([])
+    
+    useEffect(() => {
+        // have to have a useEffect to load initial list of items from the back end
+        // setPackouts(...options["data"]);
+        console.log(options["data"])
+    }, [])
+
+    const OptionsContainer = styled.div`
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-template-columns: repeat(auto-fit, minmax(300px, 100%));
+        column-gap: 5rem;
+        row-gap: 2.3rem;
+        justify-items: center;
+        // https://css-tricks.com/look-ma-no-media-queries-responsive-layouts-using-css-grid/
+        // https://css-tricks.com/snippets/css/complete-guide-grid/#aa-fluid-columns-snippet
+    `
+
+    // Need to refactor this for sp, lp and hoses
     return(
-        <div>I am the hoses</div>
+        <OptionsContainer>
+            {options.data.map((option) => (
+                <Button text={option.name} path={option.path} />
+            ))}
+        </OptionsContainer>
     )
 };
 
